203cca914c6cf4d11ce4ed0bd3cdfd5f99d541f3
kokorirko <32200702+kokorirko@users.noreply.github.com>
2020-04-21 12:32:50 +0200

    Added comments
    Comments added + js animation

--- script/main.js
+++ script/main.js
@@ -1,1 +1,11 @@
-console.log("Hello, world !");
+const soundButtonOn = document.querySelector('.fa-volume-up') // the variable that contains the fa volume up <i>
+const soundButtonOff = document.querySelector('.fa-volume-mute') // the variable that contains the fa volume mute <i>
+
+soundButtonOn.addEventListener('click', () => { // function to listen if the button is clicked
+    soundButtonOn.style.display = "none"
+    soundButtonOff.style.display ="block"
+})
+soundButtonOff.addEventListener('click', () =>{ // same but in the opposite way
+    soundButtonOff.style.display = "none"
+    soundButtonOn.style.display = "block"
+})
