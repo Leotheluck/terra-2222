a96319ef2b403e3d63031ed30a7985a319d871f3
kokorirko <32200702+kokorirko@users.noreply.github.com>
2020-04-21 18:04:10 +0200

    MUSIIIIICCC
    Added a song and the javascript coming up with. Also created a folder named "music"

--- script/main.js
+++ script/main.js
@@ -1,11 +1,14 @@
 const soundButtonOn = document.querySelector('.fa-volume-up') // the variable that contains the fa volume up <i>
 const soundButtonOff = document.querySelector('.fa-volume-mute') // the variable that contains the fa volume mute <i>
-
+const audio = new Audio('musics/song1.mp3')
 soundButtonOn.addEventListener('click', () => { // function to listen if the button is clicked
     soundButtonOn.style.display = "none"
     soundButtonOff.style.display ="block"
+    audio.pause()
 })
 soundButtonOff.addEventListener('click', () =>{ // same but in the opposite way
     soundButtonOff.style.display = "none"
     soundButtonOn.style.display = "block"
+    audio.play()
 })
+
